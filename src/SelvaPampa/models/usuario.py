57ee4dd117c6b198a84f61b8dfa0ee7dffead4cb
Ramiro Luengo <49006462+Ramiro-luengo@users.noreply.github.com>
2021-06-07 20:52:03 -0300

    Agrego metodo __str__ al usuario para poder printearlo a pantalla

--- src/SelvaPampa/models/usuario.py
+++ src/SelvaPampa/models/usuario.py
@@ -3,4 +3,7 @@ from django.db import models
 class Usuario(models.Model):
     username = models.CharField(max_length=12)
     password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
     
