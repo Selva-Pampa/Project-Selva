a3e181d9d7075fc61bb97b03c46089caf7cf856e
Ramiroluengo <85897381+Ramiroluengo@users.noreply.github.com>
2021-06-20 19:43:39 -0300

    change item views to classes

--- src/SelvaPampa/urls.py
+++ src/SelvaPampa/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views.base import item_list, users, login, item_detail
+
+from .views import item_list, users, login, item_detail
+from .views.item_views import ItemListViews, ItemViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/items/', item_list),
-    path('api/items/<int:id>/', item_detail),
+    # path('api/items/', item_list),
+    # path('api/items/<int:id>/', item_detail),
+    path('api/items/', ItemListViews.as_view()),
+    path('api/items/<int:id>/', ItemViews.as_view()),
     path('api/users/', users),
     path('api/authenticate/', login)
 ]
---
+++ src/SelvaPampa/views/item_views.py
@@ -0,0 +1,54 @@
+from django.views import View
+from django.http import HttpRequest, JsonResponse, HttpResponse
+from rest_framework.parsers import JSONParser
+
+from ..models import Item
+from ..serializers import ItemSerializer
+
+
+class ItemViews(View):
+    """
+        Esta clase permite agrupar todas las vistas
+        especificas de un item.
+    """
+    item = None
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs):
+        try:
+            self.item = Item.objects.get(id=kwargs.get('id'))
+        except Item.DoesNotExist:
+            return HttpResponse(status=404)
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, id):
+        serializer = ItemSerializer(self.item)
+        return JsonResponse(serializer.data, safe=False)
+
+    def put(self, request, id):
+        data = JSONParser().parse(request)
+        serializer = ItemSerializer(self.item, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    def delete(self, request, id):
+        self.item.delete()
+        return HttpResponse(status=204)
+
+
+class ItemListViews(View):
+
+    def get(self, request: HttpRequest):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    def post(self, request: HttpRequest):
+        data = JSONParser().parse(request)
+        serializer = ItemSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
